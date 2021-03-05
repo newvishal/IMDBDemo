7498e40198c456c8103d0b9f4a56c6ab348db8eb
11ashu <srivastavashutosh80@gmail.com>
2021-03-05 21:28:12 +0530

    validation in login form

--- src/app/login/login/login.component.ts
+++ src/app/login/login/login.component.ts
@@ -1,6 +1,7 @@
 import { DOCUMENT } from '@angular/common';
 import { Component, Inject, OnDestroy, OnInit, Renderer2 } from '@angular/core';
-import {Router} from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -8,23 +9,40 @@ import {Router} from '@angular/router';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit, OnDestroy {
-
+  loginForm: FormGroup
+  submitted = false 
   constructor (
     @Inject(DOCUMENT) private document: Document,
     private renderer: Renderer2,
-    private router: Router
+    private router: Router,
+    public fb: FormBuilder
   ) { }
 
   ngOnInit(): void {
     this.renderer.addClass(this.document.body, 'login-page');
+    this.createLoginForm();
+  }
+  
+  createLoginForm() {
+    this.loginForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    })
+  }
+
+  get getLoginForm() {
+    return this.loginForm.controls 
   }
 
   ngOnDestroy(): void {
     this.renderer.removeClass(this.document.body, 'login-page');
   }
 
-  login() {
+  onSubmit() {
+    this.submitted = true
+    if(this.loginForm.invalid) return
     this.router.navigate(['Dashboard']);
+    console.log(this.loginForm.value);
   }
 
 }
